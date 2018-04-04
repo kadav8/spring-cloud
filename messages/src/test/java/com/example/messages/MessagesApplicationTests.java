9cbe9c6fdf0e5b7834c59eea414e15b6c756a8b3
Dávid Kaszányi <kadav8@gmail.com>
2018-04-04 18:43:43 +0200

    yml to properties

--- messages/src/test/java/com/example/messages/MessagesApplicationTests.java
+++ messages/src/test/java/com/example/messages/MessagesApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example.messages;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MessagesApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private MessageSenderService senderService;
 
+	@Test
+	public void sendTest() {
+		String vid = UUID.randomUUID().toString();
+		String id = UUID.randomUUID().toString();
+		Document document = new Document();
+		document.setDocumentId(id);
+		document.setVersionSeriesId(vid);
+		document.setType("SimpleDocument");
+		Map<String, String> sp = new HashMap<>();
+		sp.put("ClientNumber", "barack12");
+		document.setStringProperties(sp);
+		senderService.send(document);
+	}
 }
